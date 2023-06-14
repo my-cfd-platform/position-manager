60286ee63daf26d3797ef89352cd5748f813aa93
RsSvc <wonderhays@gmail.com>
2023-06-14 17:34:43 +0300

    improvements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
+
+use position_manager::{
+    position_manager_grpc::position_manager_grpc_service_server::PositionManagerGrpcServiceServer,
+    AppContext, GrpcService, SettingsReader,
+};
+use service_sdk::ServiceContext;
+
+#[tokio::main]
+async fn main() {
+    let settings_reader = Arc::new(SettingsReader::new(".my-cfd").await);
+
+    let mut service_context = ServiceContext::new(settings_reader.clone());
+    let app_ctx = Arc::new(AppContext::new(&settings_reader, &service_context).await);
+    service_context.add_grpc_service(PositionManagerGrpcServiceServer::new(GrpcService::new(
+        app_ctx,
+    )));
+
+    service_context.start_application().await;
 }
--- src/settings.rs
+++ src/settings.rs
@@ -1,6 +1,7 @@
 use my_service_bus_tcp_client::MyServiceBusSettings;
 use my_settings_reader::SettingsModel;
 use serde::{Deserialize, Serialize};
+use service_sdk::ServiceInfo;
 
 #[derive(SettingsModel, Serialize, Deserialize, Debug, Clone)]
 pub struct SettingsModel {
@@ -8,6 +9,10 @@ pub struct SettingsModel {
     pub sb_tcp: String,
     #[serde(rename = "PersistenceUrl")]
     pub persistence_url: String,
+    #[serde(rename = "Seq")]
+    pub seq_conn_string: String,
+    #[serde(rename = "MyTelemetry")]
+    pub my_telemetry: String,
 }
 
 #[async_trait::async_trait]
@@ -15,5 +20,35 @@ impl MyServiceBusSettings for SettingsReader {
     async fn get_host_port(&self) -> String {
         let settings = self.get_settings().await;
         settings.sb_tcp.clone()
+    }
+}
+
+
+
+#[async_trait::async_trait]
+impl my_seq_logger::SeqSettings for SettingsReader {
+    async fn get_conn_string(&self) -> String {
+        let read_access = self.settings.read().await;
+        read_access.seq_conn_string.clone()
+    }
+}
+
+
+#[async_trait::async_trait]
+impl my_telemetry_writer::MyTelemetrySettings for SettingsReader {
+    async fn get_telemetry_url(&self) -> String {
+        let read_access = self.settings.read().await;
+        read_access.my_telemetry.clone()
+    }
+}
+
+
+#[async_trait::async_trait]
+impl ServiceInfo for SettingsReader {
+    fn get_service_name(&self) -> String {
+        env!("CARGO_PKG_NAME").to_string()
+    }
+    fn get_service_version(&self) -> String {
+        env!("CARGO_PKG_VERSION").to_string()
     }
 }
