68d811df195d2326ba5685a40e07c2d264e23125
HaysR <57479934+RomanSavh@users.noreply.github.com>
2023-10-18 16:07:41 +0300

    fixed start of tradelog

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,6 @@ async fn main() {
 
     let mut service_context = service_sdk::ServiceContext::new(settings_reader.clone()).await;
     let app_context = Arc::new(AppContext::new(&settings_reader, &service_context).await);
-    trade_log::core::TRADE_LOG.start(&service_context.sb_client).await;
     service_context.configure_grpc_server(|builder| {
         builder.add_grpc_service(PositionManagerGrpcServiceServer::new(GrpcService::new(
             app_context.clone(),
@@ -23,7 +22,7 @@ async fn main() {
     let db_job = PricesListener::new(app_context.clone());
 
     trade_log::core::TRADE_LOG.init_component_name(settings_reader.get_service_name().as_str()).await;
-
+    trade_log::core::TRADE_LOG.start(&service_context.sb_client).await;
     service_context.register_sb_subscribe(Arc::new(db_job), service_sdk::my_service_bus::abstractions::subscriber::TopicQueueType::PermanentWithSingleConnection).await;
 
     service_context.start_application().await;
