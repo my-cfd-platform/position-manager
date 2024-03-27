61d9c3ad8e07867587d41940f4c478db17ff2ffa
HaysR <57479934+RomanSavh@users.noreply.github.com>
2024-03-27 10:37:45 +0200

    fixes

--- src/grpc/client/position_manager_persistence_grpc_client.rs
+++ src/grpc/client/position_manager_persistence_grpc_client.rs
@@ -1,6 +1,6 @@
 use service_sdk::my_grpc_extensions;
 use service_sdk::my_telemetry;
-
+use service_sdk::my_logger;
 #[service_sdk::my_grpc_extensions::client::generate_grpc_client(
     proto_file: "./proto/PositionsManagerPersistence.proto",
     crate_ns: "crate::position_manager_persistence",
